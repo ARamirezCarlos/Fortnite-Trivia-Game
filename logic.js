93c2ec6a040e0540e2585e45343b1d30fb94ba4e
Ramirez <0265888@students.dadeschools.net>
2018-04-12 14:10:37 -0400

    FirstStep

---
+++ logic.js
@@ -0,0 +1,16 @@
+var panel = $ ("");
+var countStartNumber = 30;
+
+// Question set
+var questions = [{
+	questions:"What do you use to break things?",
+	answers: ["gun","pickaxe","gernade", "you cant"],
+	correctAnswer: "pickaxe",
+	image: "assets/Pickaxe.jpg",
+},
+{
+	questions:"How do you sprint on PC",
+	answers: ["LControl","Enter","LShift", "W"],
+	correctAnswer: "LControl",
+	image: "assets/LShift.png",
+}]
